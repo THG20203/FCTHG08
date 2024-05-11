0ca4075f5ff778a02af6f5cf18de574b0d642d80
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-11 15:42:06 +0100

    removal of unneeded titles

--- controllers/secondaryController.js
+++ controllers/secondaryController.js
@@ -3,43 +3,35 @@
 
 // Render pages under the "latestPages" category
 export const renderDiscover = (req, res) => {
-  res.render("secondaryPages/latestPages/discover", { title: "Discover" });
+  res.render("secondaryPages/latestPages/discover");
 };
 
 export const renderNews = (req, res) => {
-  res.render("secondaryPages/latestPages/news", { title: "News" });
+  res.render("secondaryPages/latestPages/news");
 };
 
 // Render pages under the "teamPages" category
 export const renderFixturesResults = (req, res) => {
-  res.render("secondaryPages/teamPages/fixtures-results", {
-    title: "Fixtures & Results",
-  });
+  res.render("secondaryPages/teamPages/fixtures-results");
 };
 
 export const renderLeagueTables = (req, res) => {
-  res.render("secondaryPages/teamPages/league-tables", {
-    title: "League Tables",
-  });
+  res.render("secondaryPages/teamPages/league-tables");
 };
 
 export const renderPlayersStaff = (req, res) => {
-  res.render("secondaryPages/teamPages/players-staff", {
-    title: "Players & Staff",
-  });
+  res.render("secondaryPages/teamPages/players-staff");
 };
 
 // Render pages under the "ticketPages" category
 export const renderMembership = (req, res) => {
-  res.render("secondaryPages/ticketPages/membership", { title: "Membership" });
+  res.render("secondaryPages/ticketPages/membership");
 };
 
 export const renderSeasonTickets = (req, res) => {
-  res.render("secondaryPages/ticketPages/season-tickets", {
-    title: "Season Tickets",
-  });
+  res.render("secondaryPages/ticketPages/season-tickets");
 };
 
 export const renderTickets = (req, res) => {
-  res.render("secondaryPages/ticketPages/tickets", { title: "Tickets" });
+  res.render("secondaryPages/ticketPages/tickets");
 };
