cc56e9089a3d1cb9da88538495a3c70c336101c0
THG20203 <tristanhgriffiths8@yahoo.com>
2024-05-18 20:24:20 +0100

    fixing header imports for all main pages and some secondary pages too

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,12 +2,15 @@
 // Controllers for main routes in the application
 
 export const renderHome = (req, res) =>
-  res.render("home", { pageStylePath: "main" });
+  res.render("home", { pageStylePath: "main", pageStyle: "home" });
 export const renderLatest = (req, res) =>
-  res.render("latest", { pageStylePath: "main" });
+  res.render("latest", { pageStylePath: "main", pageStyle: "latest" });
 export const renderTeam = (req, res) =>
-  res.render("team", { pageStylePath: "main" });
+  res.render("team", { pageStylePath: "main", pageStyle: "team" });
 export const renderTicketsHospitality = (req, res) =>
-  res.render("tickets-hospitality", { pageStylePath: "main" });
+  res.render("tickets-hospitality", {
+    pageStylePath: "main",
+    pageStyle: "tickets-hospitality",
+  });
 export const renderContact = (req, res) =>
-  res.render("contact", { pageStylePath: "main" });
+  res.render("contact", { pageStylePath: "main", pageStyle: "contact" });
--- controllers/secondaryController.js
+++ controllers/secondaryController.js
@@ -3,35 +3,59 @@
 
 // Render pages under the "latestPages" category
 export const renderDiscover = (req, res) => {
-  res.render("secondaryPages/latestPages/discover");
+  res.render("secondaryPages/latestPages/discover", {
+    pageStylePath: "secondary",
+    pageStyle: "discover",
+  });
 };
 
 export const renderNews = (req, res) => {
-  res.render("secondaryPages/latestPages/news");
+  res.render("secondaryPages/latestPages/news", {
+    pageStylePath: "secondary",
+    pageStyle: "news",
+  });
 };
 
 // Render pages under the "teamPages" category
 export const renderFixturesResults = (req, res) => {
-  res.render("secondaryPages/teamPages/fixtures-results");
+  res.render("secondaryPages/teamPages/fixtures-results", {
+    pageStylePath: "secondary",
+    pageStyle: "fixtures-results",
+  });
 };
 
 export const renderLeagueTables = (req, res) => {
-  res.render("secondaryPages/teamPages/league-tables");
+  res.render("secondaryPages/teamPages/league-tables", {
+    pageStylePath: "secondary",
+    pageStyle: "league-tables",
+  });
 };
 
 export const renderPlayersStaff = (req, res) => {
-  res.render("secondaryPages/teamPages/players-staff");
+  res.render("secondaryPages/teamPages/players-staff", {
+    pageStylePath: "secondary",
+    pageStyle: "players-staff",
+  });
 };
 
 // Render pages under the "ticketPages" category
 export const renderMembership = (req, res) => {
-  res.render("secondaryPages/ticketPages/membership");
+  res.render("secondaryPages/ticketPages/membership", {
+    pageStylePath: "secondary",
+    pageStyle: "membership",
+  });
 };
 
 export const renderSeasonTickets = (req, res) => {
-  res.render("secondaryPages/ticketPages/season-tickets");
+  res.render("secondaryPages/ticketPages/season-tickets", {
+    pageStylePath: "secondary",
+    pageStyle: "season-tickets",
+  });
 };
 
 export const renderTickets = (req, res) => {
-  res.render("secondaryPages/ticketPages/tickets");
+  res.render("secondaryPages/ticketPages/tickets", {
+    pageStylePath: "secondary",
+    pageStyle: "tickets",
+  });
 };
